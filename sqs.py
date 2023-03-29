f115c41170a06a7df10d60413a4db4ef271547cb
yessasikiran <sxs73800@ucmo.com>
2023-03-28 19:55:13 -0500

    update4

--- sqs.py
+++ sqs.py
@@ -7,3 +7,7 @@ config.read('my-queue.ini')
 queue_name = config['default']['queue_name']
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName=queue_name)
+
+def send_message_to_sqs(message):
+    response = queue.send_message(MessageBody=message)
+    print(f"Message sent with ID: {response['MessageId']}")
